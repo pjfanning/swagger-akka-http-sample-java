77c6ec86d9feaf9bc282dd9d4b2a4ba2bda96a2e
pj.fanning <pj.fanning@workday.com>
2017-08-16 13:34:04 +0100

    small refactor

--- src/main/java/com/example/akka/SwaggerDocService.java
+++ src/main/java/com/example/akka/SwaggerDocService.java
@@ -1,38 +1,37 @@
 package com.example.akka;
 
+import java.util.Collections;
+import java.util.Set;
+
 import akka.http.javadsl.server.AllDirectives;
 import akka.http.javadsl.server.PathMatchers;
 import akka.http.javadsl.server.Route;
 import ch.megard.akka.http.cors.javadsl.settings.CorsSettings;
-import com.github.swagger.akka.javadsl.SwaggerGenerator;
 import io.swagger.models.Info;
 
-import java.util.Collections;
-import java.util.Set;
+import com.github.swagger.akka.javadsl.SwaggerGenerator;
 
 import static ch.megard.akka.http.cors.javadsl.CorsDirectives.cors;
 
 class SwaggerDocService extends AllDirectives {
 
+  SwaggerGenerator generator = new SwaggerGenerator() {
+    @Override
+    public Set<Class<?>> apiClasses() {
+      return Collections.singleton(HttpServerMinimalExample.class);
+    }
+
+    @Override
+    public Info info() {
+      Info info = new Info().description("Simple akka-http application").version("1.0");
+      return info;
+    }
+  };
+
   Route createRoute() {
-    final Route route = route(path(PathMatchers.segment("api-docs").slash("swagger.json"), () -> get(() -> complete(swaggerJson()))));
+    final Route route = route(path(PathMatchers.segment(generator.apiDocsPath()).slash("swagger.json"),
+            () -> get(() -> complete(generator.generateSwaggerJson()))));
     final CorsSettings settings = CorsSettings.defaultSettings();
     return cors(settings, () -> route);
-  }
-
-  private String swaggerJson() {
-    SwaggerGenerator generator = new SwaggerGenerator() {
-      @Override
-      public Set<Class<?>> apiClasses() {
-        return Collections.singleton(HttpServerMinimalExample.class);
-      }
-
-      @Override
-      public Info info() {
-        Info info = new Info().description("Simple akka-http application").version("1.0");
-        return info;
-      }
-    };
-    return generator.generateSwaggerJson();
   }
 }
