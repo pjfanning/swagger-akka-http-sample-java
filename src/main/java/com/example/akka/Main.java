990945680f80d031f613154319ed4767e595b89f
PJ Fanning <pjfanning@users.noreply.github.com>
2022-03-03 21:49:34 +0100

    remove use of deprecated methods

--- src/main/java/com/example/akka/Main.java
+++ src/main/java/com/example/akka/Main.java
@@ -1,17 +1,12 @@
 package com.example.akka;
 
-import akka.NotUsed;
 import akka.actor.ActorSystem;
-import akka.http.javadsl.ConnectHttp;
 import akka.http.javadsl.Http;
 import akka.http.javadsl.ServerBinding;
-import akka.http.javadsl.model.HttpRequest;
-import akka.http.javadsl.model.HttpResponse;
 import akka.http.javadsl.server.AllDirectives;
 import akka.http.javadsl.server.Route;
-import akka.stream.ActorMaterializer;
-import akka.stream.javadsl.Flow;
 import com.example.akka.routes.Routes;
+
 import java.util.concurrent.CompletionStage;
 
 public class Main extends AllDirectives {
@@ -21,16 +16,14 @@ public class Main extends AllDirectives {
     ActorSystem system = ActorSystem.create("routes");
 
     final Http http = Http.get(system);
-    final ActorMaterializer materializer = ActorMaterializer.create(system);
 
     // In order to access all directives we need an instance where the routes
-    // are define.
+    // are defined.
     Main app = new Main();
 
     final Route routes = app.concat(new Routes().createRoute(), new SwaggerDocService().createRoute());
-    final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = routes.flow(system, materializer);
-    final CompletionStage<ServerBinding> binding = http.bindAndHandle(routeFlow, ConnectHttp
-        .toHost("localhost", 12345), materializer);
+    final CompletionStage<ServerBinding> binding = http.newServerAt("localhost", 12345)
+            .bind(routes);
 
     System.out.println("Server online at http://localhost:12345/\nPress RETURN to stop...");
     System.in.read(); // let it run until user presses return
